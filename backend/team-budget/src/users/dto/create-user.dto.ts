3faad15890267bfa917175b3404f5c5c644bd41d
David Abraham <127962371+abda1014@users.noreply.github.com>
2025-08-25 18:00:47 +0200

    added validation and repository pattern

--- backend/team-budget/src/users/dto/create-user.dto.ts
+++ backend/team-budget/src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+	@IsNotEmpty()
+	@IsString()
+	@Length(1, 100)
+	vorname: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@Length(1, 100)
+	nachname: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@Length(3, 100)
+	username: string;
+
+	@IsNotEmpty()
+	@IsEmail()
+	@Length(3, 100)
+	email: string;
+
+	@IsNotEmpty()
+	@IsString()
+	@Length(8, 128)
+	password: string;
+}
--- backend/team-budget/src/users/users.service.ts
+++ backend/team-budget/src/users/users.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Gruppe } from './entities/gruppe.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Gruppe)
+    private readonly gruppeRepsoitory: Repository<Gruppe>,
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -11,9 +21,32 @@ export class UsersService {
   findAll() {
     return `This action returns all users`;
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // gib mir den user basierend aus der gruppe
+  async findOneUser(id: string): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`User mit der #${id} wurde nicht gefunden `);
+    }
+    return user;
+  }
+  //Gib mir die gruppe des Users
+  async findGruppe(id: string): Promise<Gruppe[]> {
+    const user = await this.findOneUser(id);
+    const gruppe = user.gruppe;
+    if (!gruppe || gruppe.length === 0) {
+      throw new NotFoundException(`Es wurden keine Gruppen hinzugefügt`);
+    }
+    return gruppe;
+  }
+  //Gib mir alle User die sich in der Gruppe befinden
+  async findAllUserinGruppe(id: string): Promise<User[]> {
+    const gruppe = await this.gruppeRepsoitory.findOneBy({ id });
+    if (!gruppe || !gruppe.users || gruppe.users.length === 0) {
+      throw new NotFoundException(
+        `Es wurden keine user gefunden zur Gruppen id #${id}`,
+      );
+    }
+    return gruppe.users;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
